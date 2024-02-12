399ee19a8826cb7311c4f4d9c640cc5f5304b20d
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-02-12 17:43:26 +0530

    ..

--- Tejal/main.js
+++ Tejal/main.js
@@ -1,7 +1,8 @@
 
+let array=[]
 
 // add a dyanamic date in date portion
-let cdate=document.getElementById("cdate")
+let cdate=document.getElementById("cDate")
 
 let date = new Date();
 	let day1 = date.getDate();
@@ -18,56 +19,56 @@ let date = new Date();
 cdate.innerText=currentDate
 
 // add detail in card dynamically
-let details=document.getElementById("added")
-let itemCounter=0;
-let addDetails =()=>{
-    itemCounter++;
+// let details=document.getElementById("added")
+// let itemCounter=0;
+// let addDetails =()=>{
+//     itemCounter++;
     
-console.log("updated");
-let third=document.createElement('div')
-third.classList.add("third")
-third.style.marginBottom="25px"
+// console.log("updated");
+// let third=document.createElement('div')
+// third.classList.add("third")
+// third.style.marginBottom="25px"
 
 
-third.innerHTML=`
+// third.innerHTML=`
 
-<div class="third_left">
+// <div class="third_left">
 
-<input type="text" placeholder="Item Name">
-<input type="text" placeholder="Item Description">
+// <input type="text" placeholder="Item Name">
+// <input type="text" placeholder="Item Description">
 
-</div>
-<div class="third_right">
-<div class="qty">
+// </div>
+// <div class="third_right">
+// <div class="qty">
     
-    <input type="number">
-</div>
-<div class="price">
+//     <input type="number">
+// </div>
+// <div class="price">
     
-    <input type="number">
-</div>
-<div class="action">
+//     <input type="number">
+// </div>
+// <div class="action">
 
-    <img id="trash" src="./images/trash.png" alt="" onclick="removeItem(${itemCounter})">
-</div>
+//     <img id="trash" src="./images/trash.png" alt="" onclick="removeItem(${itemCounter})">
+// </div>
 
-</div>
-
-
+// </div>
 
 
-`
-details.append(third)
-
-updateAmount();
-}
 
 
-document.getElementById("add-items").addEventListener("click", (e)=> {
-    e.preventDefault();
-    addDetails();
+// `
+// details.append(third)
+
+// updateAmount();
+// }
+
+
+// document.getElementById("add-items").addEventListener("click", (e)=> {
+//     e.preventDefault();
+//     addDetails();
     
-});
+// });
 
 
 
@@ -75,33 +76,53 @@ document.getElementById("add-items").addEventListener("click", (e)=> {
 
 document.getElementById("add-row").addEventListener("click", (e)=> {
     e.preventDefault();
-    addNewRow();
+    // addNewRow();
 });
  
 
 // demo
 
 const tBody = document.getElementById("table-body");
-
-// let addNewRow =()=> {
-//     const row = document.createElement("tr");
-//     row.className = "single-row";
-//     row.innerHTML = `<td><input type="text" placeholder="Product name" class="product" id="product"></td>
-//                     <td><input type="number" placeholder="0" name="unit" class="unit" id="unit" onkeyup="getInput()"></td>
-//                     <td><input type="number" placeholder="0" name="price" class="price" id="price" onkeyup="getInput()"></td>
-//                     <td><input type="number" placeholder="0" name="amount" class="amount" id="amount" disabled></td>
-//                     <td style="text-align: right;" id="imgg"><span id="img"><img id="image"
-//                     src="./images/trash.png" alt="delete_outline" action="delete"></span></td>`
-
-//     tBody.insertBefore(row, tBody.lastElementChild.previousSibling);
-// }
-
-// document.getElementById("add-row").addEventListener("click", (e)=> {
-//     e.preventDefault();
-//     // addNewRow();
-// });
+let arrays=[];
+let addNewRow =()=> {
+    const row = document.createElement("tr");
+    row.className = "single-row";
+    row.innerHTML = `<td><input type="text" placeholder="Product name" class="product" id="product"></td>
+                    <td><input type="number" placeholder="0" name="unit" class="unit" id="unit" onkeyup="getInput()"></td>
+                    <td><input type="number" placeholder="0" name="price" class="price" id="price" onkeyup="getInput()"></td>
+                    <td><input type="number" placeholder="0" name="amount" class="amount" id="amount" disabled></td>
+                    <td style="text-align: right;" id="imgg"><span id="img"><img id="image"
+                    src="./images/trash.png" alt="delete_outline" action="delete"></span></td>
+                    `
+    // tBody.insertBefore(row, tBody.lastElementChild.previousSibling);
+    tBody.append(row)
 
 
+    let product=document.querySelector(".product")
+    let quantity=document.querySelector(".unit")
+    let prices=document.querySelector(".price")
+    let amt=document.querySelector(".amount")
+
+    // let newRow={
+    //     product:"",
+    //     price:"",
+    //     Quantity:"",
+    //     amount:""
+    // }
+
+
+
+    
+
+    
+
+}
+
+// array.push(addNewRow())
+// console.log(array.length);
+
+
+//getting the
 getInput =()=> {
     var rows = document.querySelectorAll("tr.single-row");
     rows.forEach((currentRow) => {
@@ -144,7 +165,7 @@ delRow =(el)=> {
 }
 
 
-// print receipt
+// // print receipt
 
 const btnPrint = document.querySelector(".print");
 btnPrint.addEventListener("click", () => {
@@ -159,31 +180,94 @@ var price;
 var quantity;
 var amount;
 var prod_name;
-var ids;
-var tax = 50;
+
 
 function calculate() {
 //   ids = document.getElementById("ids").value;
+  prod_name = document.getElementById("product").value;
   price = document.getElementById("price").value;
   quantity = document.getElementById("unit").value;
   amount = price * quantity;
   document.getElementById("amount").value = amount;
+  document.getElementById("product").value=prod_name;
+  document.getElementById("price").value=price
+  document.getElementById("unit").value
 
 }
 
 
 function addData() {
-  prod_name = document.getElementById("product").value;
-  price=document.getElementById("price").value
-  quantity=document.getElementById("unit").value
-  document.getElementById('newtr').innerHTML += '<tr><td>'  + prod_name + '</td><td>' + price + '</td> <td>' + quantity + '</td><td>' + amount + '</td></tr>'
+    let tableBody=document.getElementById("table-body");
+    console.log(tableBody.children);
+    for (let i = 0; i < tableBody.children.length; i++) {
+        // const element = array[i];
+        tableBody.innerHTML += '<tr><td>'  + prod_name + '</td><td>' + price + '</td> <td>' + quantity + '</td><td>' + amount + '</td></tr>'
+        
+    }
+    
+//   prod_name = document.getElementById("product").value;
+//   price=document.getElementById("price").value
+//   quantity=document.getElementById("unit").value
+//   document.getElementById('newtr').innerHTML += '<tr><td>'  + prod_name + '</td><td>' + price + '</td> <td>' + quantity + '</td><td>' + amount + '</td></tr>'
 
-  document.getElementById("product").value = "";
-  document.getElementById("price").value = "";
-  document.getElementById("unit").value = "";
-  document.getElementById("amount").value = "";
+ 
+//   document.getElementById("total-amount").innerHTML = price * quantity - tax;
+//   let total_answer=document.getElementById("total").value
+
+//    document.getElementsByName("subtotal-amount").innerText=total_answer;
+  
+  
+}
+
+
+// const divi=document.querySelector(".single-row")
+// console.log(divi.children);
+// array.push(divi.children)
+// console.log(array);
+// array.forEach((value) => {
+
+// // value.price
+// console.log(value.price);
+// })
+
+
+
+
+
+
+// addData =()=> {
+//     var rows = document.getElementById("newtr");
+//     rows.forEach((currentRow) => {
+//         var unit = currentRow.querySelector("#unit").value;
+//         var price = currentRow.querySelector("#price").value;
+//         prod_name = document.querySelector("#product").value;
+//         document.getElementById('newtr').innerHTML += '<tr><td>'  + prod_name + '</td><td>' + price + '</td> <td>' + unit + '</td><td>' + amount + '</td></tr>'
+
+//         // amount = unit * price;
+//         // currentRow.querySelector("#amount").value = amount;
+//         // overallSum();
+
+//     })
+// };
+
+
+
+function share(){
+    let modal=document.getElementById("myModal")
+    
+    window.open("whatsapp://send?text="+"model"+modal.innerHTML ,"twitter window","width=600","height=300");
 
 }
+
+
+// let itemAdded=document.getElementById("added")
+
+// let data4=document.getElementById("dataAdded")
+// console.log(data4);
+
+// let amt=document.getElementById("total").value
+// document.getElementById("amount_total").innerText=amt
+
 
 
 
