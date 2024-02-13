8e6dc706b050e10bb03c97e1608f874832d447c3
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-02-13 13:17:23 +0530

    invoice

--- Tejal/data.js
+++ Tejal/data.js
@@ -3,61 +3,48 @@
 let review=document.getElementById("review")
 review.addEventListener("click",()=>{
 
-    // this data is about bill to and bill from
+ 
 
 let whom2=document.getElementById("whom2").value;
-document.getElementById("whom").value=whom2
+document.getElementById("whom").innerText=whom2
 
 let from=document.getElementById("from").value;
-document.getElementById("from3").value=from
+document.getElementById("from3").innerText=from
 
 
 let mailTo=document.getElementById("to2").value
-document.getElementById("idTo").value=mailTo
+document.getElementById("idTo").innerText=mailTo
 
 
 
 let addTo=document.getElementById("address_to").value
-document.getElementById("addTo").value=addTo
+document.getElementById("addTo").innerText=addTo
 
 let addFrom=document.getElementById("FromAdd").value
-document.getElementById("billAdd").value=addFrom
+document.getElementById("billAdd").innerText=addFrom
 
 
 let billFrom=document.getElementById("from").value
 
 let mailFrom=document.getElementById("fromMail").value
-document.getElementById("billmail").value=mailFrom
+document.getElementById("billmail").innerText=mailFrom
 
 // this data is for 1st part invoice number and date
 let vNumber=document.getElementById("voice").value
-document.getElementById("voice2").value=vNumber
+document.getElementById("voice2").innerText=vNumber
 
 //fetch tha value for current date
 let currentDate=document.getElementById("cDate").innerText
 document.getElementById("cdate").innerText=currentDate
 
-//now data fetch for due date
 
-// let due_date=document.getElementById("dDate").value
-// document.getElementById("due_date").value=new Date(due_date)
-
+//due date value
 
 
 })
 
 
 
-// let review=document.getElementById("review")
-// review.addEventListener("click",()=>{
-   
 
-//     let whom2=document.getElementById("whom2").value;
-// document.getElementById("whom").value=whom2
-
-
-
-
-// })
 
 
--- Tejal/demo.js
+++ Tejal/demo.js
@@ -1,86 +1,163 @@
-// this data is for 1st part invoice number and date
-let vNumber=document.getElementById("voice")
 
 
-//fetch tha value for current date
-let currentDate=document.getElementById("cDate")
+
+let array=[]
+
+// add a dyanamic date in date portion
+let cdate=document.getElementById("cDate")
+
+let date = new Date();
+	let day1 = date.getDate();
+	let month = date.getMonth()+1;
+	let year = date.getFullYear();
+
+  let currentDate =
+		day1 +
+		" /" +
+		month + 
+		" /" +
+		year ; 
+
+cdate.innerText=currentDate
 
 
-let whom2=document.getElementById("whom2");
-// bill to section values
-let from=document.getElementById("from");
-let mailTo=document.getElementById("to2")
-let addTo=document.getElementById("address_to")
 
-// bill from section values
-let billFrom=document.getElementById("from")
-let mailFrom=document.getElementById("fromMail")
-let addFrom=document.getElementById("FromAdd")
+document.getElementById("add-row").addEventListener("click", (e)=> {
+    e.preventDefault();
+    // addNewRow();
+});
+ 
 
+// demo
 
-let itemToBeAdded=document.getElementById("added")
-let allItemsDetails = [];
-
-let addItem=document.getElementById("add-row")
-
-addItem.addEventListener("click", createItem);
-function createItem(newItem){
-    if (newItem?.length !== undefined) {
-        allItemsDetails = newItem;
-      }else{
-        allItemsDetails.push({
-          name: "",
-          des: "",
-          qty: 1,
-          price: 1,
-        });
+const tBody = document.getElementById("table-body");
+// let arrays=[];
+let addNewRow =()=> {
+    const row = document.createElement("tr");
+    row.className = "single-row";
+    row.innerHTML = `<td><input type="text" placeholder="Product name" class="product" id="product"></td>
+                    <td><input type="number" placeholder="0" name="unit" class="unit" id="unit" onkeyup="getInput()"></td>
+                    <td><input type="number" placeholder="0" name="price" class="price" id="price" onkeyup="getInput()"></td>
+                    <td><input type="number" placeholder="0" name="amount" class="amount" id="amount" disabled></td>
+                    <td style="text-align: right;" id="imgg"><span id="img"><img id="image"
+                    src="./images/trash.png" alt="delete_outline" action="delete"></span></td>
+                    `
+    // tBody.insertBefore(row, tBody.lastElementChild.previousSibling);
+     tBody.append(row)
     
-      // calculateTotalSub();
-      addItem.innerHTML = "";
-  allItemsDetails.map((item, idx) => {
-    let div=document.createElement('div')
-    div.classList.add("third")
-    div.innerHTML=`
-    <div class="third_left">
-                                <p>ITEMS</p>
-                                <input type="text" placeholder="Item Name">
-                                 <input type="text" placeholder="Item Description"> 
 
-                            </div>
-                            <div class="third_right" style="display: flex;gap: 20%;">
-
-                                <div class="qty">
-                                    <p>QTY</p>
-                                    <input type="number" id="unit">
-                                </div>
-                                <div class="price">
-                                    <p>PRICE/ITEMS</p>
-                                    <input type="number" id="price">
-                                </div>
-                                <div class="action">
-                                    <p>ACTION</p>
-                                    <img id="trash" src="./images/trash.png" alt="">
-                                </div>
-
-                            </div>
+    //     prod_name=document.getElementById("product").value
+    // price=document.getElementById("price").value
+    // quantity=document.getElementById("unit").value
+  
     
+    // document.getElementById('newtr').innerHTML += '<tr><td>'  + prod_name + '</td><td>' + price + '</td> <td>' + quantity + '</td><td>' + amount + '</td></tr>'
+  
+   
+    // document.getElementById("total").innerHTML = price * quantity ;
+    // let total_answer=document.querySelector("#invoice .#sum #total").value
+  
+    //  document.getElementById("total").innerText=total_answer;
+    //  document.getElementById("finalAmount").innerText=total_answer;
+        console.log(tBody.children);
     
-    `
-    let priceRate=document.querySelector("#price")
-    priceRate.value = item.price;
-    priceRate.addEventListener("input", () => {
-      item.price= priceRate.value;
-      // calculateTotalSub();
-    });
-
-    itemToBeAdded.append(div)
-  })
-
-
-  console.log(allItemsDetails.price);
-        
-      }
+   
 }
+
+// array.push(addNewRow())
+// console.log(array.length);
+
+
+//getting the
+getInput =()=> {
+    var rows = document.querySelectorAll("tr.single-row");
+    rows.forEach((currentRow) => {
+        var unit = currentRow.querySelector("#unit").value;
+        var price = currentRow.querySelector("#price").value;
+
+        amount = unit * price;
+        currentRow.querySelector("#amount").value = amount;
+        overallSum();
+
+    })
+};
+
+
+overallSum =()=> {
+    var arr = document.getElementsByName("amount");
+    var total = 0;
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i].value) {
+            total += +arr[i].value;
+        }
+        document.getElementById("total").value = total;
+        let subTotal=document.getElementById("subtotal-amount")
+        subTotal.innerText=total
+
+        let lastamount=document.getElementById("lastamount")
+        lastamount.innerText=total
+       
+    }
+}
+
+//Delete row from the table
+tBody.addEventListener("click", (e)=>{
+    let el = e.target;
+    const deleteROW = e.target.getAttribute("action");
+    if(deleteROW == "delete") {
+        delRow(el);
+        overallSum();
+    }
+})
+
+//Target row and remove from DOM;
+delRow =(el)=> {
+    el.parentNode.parentNode.parentNode.parentNode.removeChild(el.parentNode.parentNode.parentNode);
+}
+
+
+// // print receipt
+
+const btnPrint = document.querySelector(".print");
+btnPrint.addEventListener("click", () => {
+    // let model=document.getElementById("model")
+console.log(window);
+    window.print();
+});
+
+
+// data to be added in model
+var price;
+var quantity;
+var amount;
+var prod_name;
+
+
+function calculate() {
+//   ids = document.getElementById("ids").value;
+  prod_name = document.getElementById("product").value;
+  price = document.getElementById("price").value;
+  quantity = document.getElementById("unit").value;
+  amount = price * quantity;
+  document.getElementById("amount").value = amount;
+  document.getElementById("product").value=prod_name;
+  document.getElementById("price").value=price
+  document.getElementById("unit").value
+
+}
+
+function share(){
+    let modal=document.getElementById("myModal")
+    let lastamount=document.getElementById("lastamount").value
+    
+    
+    window.open("whatsapp://send?text="+"Total Amount Is : "+lastamount,"twitter window","width=600","height=300");
+
+}
+
+
+
+
 
 
 
